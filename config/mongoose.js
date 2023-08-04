5d981bdea0832e8cb035ebbce65ffe349b03d597
lovekesh <xvskd@gmail.com>
2023-08-04 21:33:23 +0530

    docker-compose mongodb connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 // mongoose.connect('mongodb://0.0.0.0:27017/UsersInfo');
-mongoose.connect('mongodb://admin:password@mongodb:27017/UserInfo');
+mongoose.connect('mongodb://admin:password@mongodb:27017/UserInfo?directConnection=true&authSource=admin');
 const db=mongoose.connection;
 
 db.on('error',console.error.bind(console,"Error in connecting"));
