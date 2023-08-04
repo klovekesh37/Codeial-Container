3b7a48a79d90fa7dd5e5329b13d8789815ce5eda
lovekesh <xvskd@gmail.com>
2023-08-04 18:28:59 +0530

    fesfas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 // mongoose.connect('mongodb://0.0.0.0:27017/UsersInfo');
-mongoose.connect('mongodb://admin:password@mongodb');
+mongoose.connect('mongodb://admin:password@mongodb:27017/UserInfo');
 const db=mongoose.connection;
 
 db.on('error',console.error.bind(console,"Error in connecting"));
